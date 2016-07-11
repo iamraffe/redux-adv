2e5e469526de83e6189ccf5c981561f03f7804ec
Rafa Ramirez <raffe90@gmail.com>
2016-07-11 21:01:19 +0200

    Adds router refactor & Header component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import 'react-polyfill'
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import './styles/styles.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import routes from './route'
 
 render(
   <Router history={browserHistory} routes={routes} />,
