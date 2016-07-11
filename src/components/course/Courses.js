171f7d2b45605eeb97d635e334d19918005e077a
Rafa Ramirez <raffe90@gmail.com>
2016-07-12 00:18:19 +0200

    Adds a complete basic Redux Flow

--- src/components/course/Courses.js
+++ src/components/course/Courses.js
@@ -1,14 +1,74 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
+import { connect } from 'react-redux'
+import * as courseActions from '../../actions/courseActions'
+import { bindActionCreators } from 'redux'
 
 class Courses extends React.Component{
+  constructor(props, context){
+    super(props, context)
+    
+    this.state = {
+      course: { title: "" }
+    }
+    this.onTitleChange = this.onTitleChange.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+  }
+
+  onTitleChange(e){
+    const course = this.state.course
+    course.title = e.target.value
+    this.setState({course: course})
+  }
+
+  onClickSave(){
+    this.props.actions.createCourse(this.state.course)
+  }
+
+  courseRow(course,index){
+    return (
+      <div key={index}>
+        {course.title}
+      </div>
+    )
+  }
+
   render(){
     return(
       <div>
-      CoursesPage
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}
+        />
       </div>
     )
   }
 }
 
-export default Courses
+Courses.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state, ownProps){
+  return {
+    courses: state.courses
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    actions: bindActionCreators(courseActions, dispatch) 
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Courses)
